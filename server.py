43d7f15b1ef39a0e947253c075b930d6b7938c70
KuborKelp <78284261+KuborKelp@users.noreply.github.com>
2023-07-09 23:55:59 +0800

    Update server.py

--- server.py
+++ server.py
@@ -11,7 +11,10 @@ from time import sleep
 IP = '172.23.176.80'
 PORT = 5555     # 端口
 users = []   # 0:userName 1:status
-guess_owner = None
+
+guess_owner = [None, None]
+guess_status = None
+
 lock = threading.Lock()
 
 def onlines():    # 统计当前在线人员
@@ -45,6 +48,7 @@ class ChatServer(threading.Thread):
         # USERS = onlines()
         # # 在获取用户名后便会不断地接受用户端发来的消息（即聊天内容），结束后关闭连接。
         user = None
+        global guess_owner,guess_status
         try:
             while True:
                 msg = json.loads(conn.recv(1024).decode('utf-8'))
@@ -62,22 +66,30 @@ class ChatServer(threading.Thread):
                         case 'guess_join':
                             users[j][1] = 'guess_join'
                         case 'guess_players_update':
-                            print('shit')
                             message = []
+                            owner_alive = False
                             for i in users:
                                 if i[1] == 'guess_join':
-                                    u = i[0]
-                                    message.append(u)
-                            if guess_owner not in message:                              
-                                guess_owner = random.randint(0, len(message))
-                            print(guess_owner)
-                            message = ['[房主]'+message[guess_owner]]+message[0:guess_owner]+message[guess_owner:]
-                            print(message)
-                            msg = {'event':'return_guess_players','args':[message]}
-                            print(msg)
+                                    message.append(i[0])
+                                    if i[0] == guess_owner[1]:
+                                        owner_alive = True
+                            if not owner_alive:                         
+                                temp = random.randint(0, len(message))
+                                guess_owner = [temp, message[0]]
+                            message.pop(guess_owner[0])
+                            message = ['[房主]'+guess_owner[1]]+message
+                            print('guess',message)
+                            msg = {'event':'return_guess_players','args':message}
                             msg = json.dumps(msg).encode('utf-8')
-                            print(msg)
                             conn.send(msg)
+                        case 'guess_start':
+                            guess_status = 1
+                            msg = {'event':'guess_start','args':[None]}
+                            msg = json.dumps(msg).encode('utf-8')
+                            conn.send(msg)
+                            pass
+                        case 'guess_status_update':
+                            pass
                                 
                 print(users)
 
